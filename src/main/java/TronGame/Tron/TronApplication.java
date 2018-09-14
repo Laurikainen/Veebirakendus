2d5fef4b01e20b84fb31e5f111810cc42ce054ef
KaarelRyysak <kaarelgames@ryysak.com>
2018-09-14 12:00:09 +0300

    basic spring initializr files

---
+++ src/main/java/TronGame/Tron/TronApplication.java
@@ -0,0 +1,12 @@
+package TronGame.Tron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TronApplication.class, args);
+	}
+}
---
+++ src/test/java/TronGame/Tron/TronApplicationTests.java
@@ -0,0 +1,16 @@
+package TronGame.Tron;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TronApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
