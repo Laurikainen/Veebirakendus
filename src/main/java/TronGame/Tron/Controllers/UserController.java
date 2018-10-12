15f5c3bf98ac6080951733cf71072dcab79c145e
Ingrid Laurikainen <ingridlaurikainen@gmail.com>
2018-10-12 08:45:28 +0300

    Working registration and user info

--- src/main/java/TronGame/Tron/Controllers/UserController.java
+++ src/main/java/TronGame/Tron/Controllers/UserController.java
@@ -5,16 +5,11 @@ import TronGame.Tron.Entities.UploadForm;
 import TronGame.Tron.Repositories.PictureRepository;
 import TronGame.Tron.Repositories.RegistrationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 import java.util.*;
@@ -33,22 +28,25 @@ public class UserController {
         Map<String, Object> data = (Map<String, Object>) ((OAuth2Authentication) principal).getUserAuthentication().getDetails();
         RegistrationForm user = registrationRepository.findByPrincipal((String) data.get("id"));
         if (user == null) {
-            //registrationRepository.register((String) data.get("id"), (String) data.get("name"), (String) data.get("locale"));
+            registrationRepository.register((String) data.get("id"), (String) data.get("name"), (String) data.get("locale"));
         }
 
-        List<Object> list = registrationRepository.joinByPrincipal((String) data.get("id"));
-        System.out.println(list);
-        for (int i=0; i<list.size(); i++) {
-            System.out.println(i);
-        }
+        String str = registrationRepository.joinByPrincipal((String) data.get("id"));
+        String[] pieces = str.split(",");
+
+        model.addAttribute("id", pieces[0]);
+        model.addAttribute("name",pieces[1]);
+        model.addAttribute("locale", pieces[2]);
+        model.addAttribute("picture_name", pieces[3]);
+        model.addAttribute("type", pieces[4]);
 
         Optional<UploadForm> uploadForm = pictureRepository.findById((String) data.get("id"));
         if (uploadForm.isPresent()) {
-            model.addAttribute("uploadForm", uploadForm.get()); //Pole vaja
+            //model.addAttribute("uploadForm", uploadForm.get()); //Pole vaja
             byte[] array = uploadForm.get().getData();
             String base64 = "data:" + uploadForm.get().getType() + ";base64, " + Base64Utils.encodeToString(array);
             model.addAttribute("image", base64);
-            return "picture";
+            return "user";
         }
         return "upload";
     }
--- src/main/java/TronGame/Tron/Repositories/RegistrationRepository.java
+++ src/main/java/TronGame/Tron/Repositories/RegistrationRepository.java
@@ -7,17 +7,20 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public interface RegistrationRepository extends CrudRepository<RegistrationForm, Integer> {
 
     @Modifying
+    @Transactional
     @Query(value = "INSERT INTO users (id, name, locale) VALUES (:id, :name, :locale)", nativeQuery = true)
-    RegistrationForm register(@Param("id") String id, @Param("name") String name, @Param("locale") String locale);
+    void register(@Param("id") String id, @Param("name") String name, @Param("locale") String locale);
 
-    @Query(value = "SELECT * FROM users JOIN profile ON users.id=profile.username WHERE users.id=(:id)", nativeQuery = true)
-    List<Object> joinByPrincipal(@Param("id") String id);
+    @Query(value = "SELECT users.id, users.name, users.locale, profile.title, profile.type " +
+            "FROM users JOIN profile ON users.id=profile.username WHERE users.id=(:id)", nativeQuery = true)
+    String joinByPrincipal(@Param("id") String id);
 
     @Query(value = "SELECT * FROM users WHERE id=(:id)", nativeQuery = true)
     RegistrationForm findByPrincipal(@Param("id") String id);
