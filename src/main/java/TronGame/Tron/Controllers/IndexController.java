132c732dbb8660f98b16298a64e959054e1cbfcd
Ingrid Laurikainen <ingridlaurikainen@gmail.com>
2018-12-02 21:15:43 +0200

    Redirect solution? 3

--- src/main/java/TronGame/Tron/Controllers/IndexController.java
+++ src/main/java/TronGame/Tron/Controllers/IndexController.java
@@ -67,7 +67,7 @@ public class IndexController {
             model.addAttribute("sudoku", base64);
         }
 
-        return "forward:https://demozone-e2d6.kxcdn.com/main_page";
+        return "forward:https://demozone-e2d6.kxcdn.com/";
     }
 
     @RequestMapping("/login")
